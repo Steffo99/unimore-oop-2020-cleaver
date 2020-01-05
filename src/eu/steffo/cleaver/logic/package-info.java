872a993c1bc761fdf7dab4d993039135ecae615d
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-01-05 16:58:06 +0100

    Update docs once again

--- src/eu/steffo/cleaver/logic/package-info.java
+++ src/eu/steffo/cleaver/logic/package-info.java
@@ -1,4 +1,9 @@
 /**
  * A package containing the core logic of the program.
+ *
+ * @see eu.steffo.cleaver.logic.compress
+ * @see eu.steffo.cleaver.logic.crypt
+ * @see eu.steffo.cleaver.logic.split
+ * @see eu.steffo.cleaver.logic.progress
  */
 package eu.steffo.cleaver.logic;
--- src/eu/steffo/cleaver/package-info.java
+++ src/eu/steffo/cleaver/package-info.java
@@ -1,5 +1,9 @@
 /**
  * The main package of the program.
  * It contains all the runnable functions (currently only {@link eu.steffo.cleaver.Main Main}), and all other packages.
+ *
+ * @see eu.steffo.cleaver.logic
+ * @see eu.steffo.cleaver.gui
+ * @see eu.steffo.cleaver.errors
  */
 package eu.steffo.cleaver;
